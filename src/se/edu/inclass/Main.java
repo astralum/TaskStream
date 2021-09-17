01a3d6bb9d93613e7399f4532e8e8017bd41329f
astralum <minyue.j@gmail.com>
2021-09-17 17:19:28 +0800

    Add method to print deadlines using streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -59,4 +59,11 @@ public class Main {
             }
         }
     }
+
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing deadlines using streams");
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .forEach(System.out::println);
+    }
 }
